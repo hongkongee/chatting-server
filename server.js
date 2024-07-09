7d03e8f95bf247bb1e51f403688f20d114073f1b
jeongwon <tjtkdvl@gmail.com>
2024-07-09 11:23:21 +0900

    서버 주소 수정

--- server.js
+++ server.js
@@ -177,5 +177,5 @@ io.sockets.on('connection', (socket) => {
 
 const port = 5000;
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
